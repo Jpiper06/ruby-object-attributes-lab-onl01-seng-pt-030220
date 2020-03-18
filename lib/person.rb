dc11f86f5f491b9d4e4e87e68647744f53385c63
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-18 19:49:42 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+Class Person
+
+  def name=(name)
+    @name = name 
+  end 
+  
+  def name 
+    @name
+  end 
+  
+  def job=(job)
+    @job = job 
+  end 
+  
+  def job
+    @job 
+  end 
+  
+
+  
